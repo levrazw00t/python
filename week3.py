536eeb08b958dc8e55e49e40737e0b1237767371
levrazw00t <138672216+levrazw00t@users.noreply.github.com>
2023-07-26 15:20:47 +0300

    Update week3.py

--- week3.py
+++ week3.py
@@ -9,10 +9,9 @@ import hashlib
 h2={'md5':(hashlib.md5(h.encode())).hexdigest(),
     'sha1':(hashlib.sha1(h.encode())).hexdigest(),
     'sha224':(hashlib.sha224(h.encode())).hexdigest(),
-    'sha224':(hashlib.sha224(h.encode())).hexdigest(),
     'sha256':(hashlib.sha256(h.encode())).hexdigest(),
     'sha384':(hashlib.sha384(h.encode())).hexdigest(),
-    'sha512':(hashlib.sha224(h.encode())).hexdigest()}
+    'sha512':(hashlib.sha512(h.encode())).hexdigest()}
 
 for key, value in sorted(h2.items()):
     print(key, value)
