5f6eb8e7da3e6fbf29fcb4d8b5b95349a0b5ff9e
levrazw00t <138672216+levrazw00t@users.noreply.github.com>
2024-07-10 00:45:48 +0300

    Update week6.py
    relis

--- week6.py
+++ week6.py
@@ -1,110 +1,65 @@
-head=input()
-color_Title1=input()
-color_text=input()
-Title1=input()
-text=input()
-
-def makehtml(fn):                                            
-  def inner(*args,**kwargs):
-    print ("<html>")
-    print (fn(*args,**kwargs))
-    print("</html>")
-  return inner
-def makehead(fn):                                            
-  def inner(*args,**kwargs):
-    print ("<head>")
-    print (fn(*args,**kwargs))
-    print("</head>")
-  return inner  
-def maketitle(fn):
-  def inner(*args,**kwargs):
-    print ("<title>" + fn(*args,**kwargs) + "</title>")
-  return inner  
-def makestyle(fn):
-  def inner(*args,**kwargs):
-    print ("<style>" + fn(*args,**kwargs) + "</style>")
-  return inner  
-def makebody(fn):
-  def inner(*args,**kwargs):
-    print ("<body>")
-    print (fn(*args,**kwargs))
-    print ("</body>")
-  return inner
-def makeh1(fn):
-  def inner(*args,**kwargs):
-    print ("<h1>" + fn(*args,**kwargs) + "</h1>")
-  return inner
-def makep(fn):
-  def inner(*args,**kwargs):
-    print ("<p>" + fn(*args,**kwargs) + "</p>")
-  return inner
-
-@makehtml
-@makehead
-@maketitle
-@makestyle
-def h0(text='hello'):
-  return text
-
-
-
-  
-
-
-
-
->>> def p(func):
-...     def inner(text):
-...         print('<p>')
-...         func(text)
-...         print('</p>')
-...     return inner
-...
->>> def li(func):
-...     def inner(text):
-...         print('<li>')
-...         func(text)
-...     return inner
-...
->>> @p
-... @li
-... def hello(text):
-...     print ('Hello %s'%text)
-...
->>> hello('Python')
-<p>
-<li>
-Hello Python
-</p>
-
-
-
-
-  
-
-
-
-def makehtml(fn):                                            #применение декораторов, возвращающих значение
-  def inner(*args,**kwargs):
-    return "<html>" + fn(*args,**kwargs) + "</html>"
-  return inner
-def makebody(fn):
-  def inner(*args,**kwargs):
-    return "<body>" + fn(*args,**kwargs) + "</body>"
-  return inner
-def maketitle(fn):
-  def inner(*args,**kwargs):
-    return "<title>" + fn(*args,**kwargs) + "</title>"
-  return inner  
-@makehtml
-def h0(text='hello'):
-  @maketitle
-  def h1(text='title'):
+def mydecor1(tag):
+    def wrapper(func_todecor):
+        def inner(*args, **kwargs):
+            return "<{0}>\n{1}</{0}>\n".format(tag, func_todecor(*args, **kwargs))
+        return inner
+    return wrapper
+ 
+def mydecor2(tag):
+    def wrapper(func_todecor):
+        def inner(*args, **kwargs):
+            return "<{0}>{1}</{0}>\n".format(tag,func_todecor(*args, **kwargs))
+        return inner
+    return wrapper
+ 
+#return <html>
+@mydecor1("html")
+def decor_html(*args):
+    return"{}{}".format(*args)
+ 
+#return <head>
+@mydecor1("head")
+def decor_head(*args):
+    return "{}{}".format(*args)
+ 
+#return <title>
+@mydecor2("title")
+def decor_title(text):
     return text
-  @makebody
-  def h2(text='body'):
-    return text
-  text=str(h1())+str(h2())
-  return text
-print (h0())  
-<html><title>title</title><body>body</body></html>
+ 
+#return <style>
+@mydecor2("style")
+def concat_style(func_1, func_2):
+    return "{0}{1}".format(func_1, func_2)
+ 
+def decor_style(*args, **kwargs):
+    return "{0}".format(*args)+"{color:" + "{1}".format(*args)+"}"
+ 
+#return <body>
+@mydecor1("body")
+def decor_body(*args):
+    return "{}{}".format(*args)
+ 
+@mydecor2("h1")
+def head1(*args):
+    return "{}".format(*args)
+ 
+@mydecor2("p")
+def paragraph(*args):
+    return "{}".format(*args)
+ 
+greeting = input()
+blue = input()
+red = input()
+title = input()
+article = input()
+ 
+ 
+hello = decor_title(greeting)
+style_decor = concat_style(decor_style("h1",blue), decor_style("p",red))
+head = decor_head(hello, style_decor)
+head2 = head1(title)
+parag = paragraph(article)
+body = decor_body(head2, parag)
+final = decor_html(head, body)
+print(final)
