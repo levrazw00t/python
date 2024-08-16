d652c4077b72693f330be867b5f62013bfa5e53f
levrazw00t <138672216+levrazw00t@users.noreply.github.com>
2024-08-16 13:10:28 +0300

    Update week8.py

--- week8.py
+++ week8.py
@@ -11,7 +11,42 @@
 #('http', 'en.ifmo.ru', '/en/contacts/Contacts.htm', '', '', '')
 #Contacts
 
-
+import urllib
+import urllib.request
+import urllib.parse
+from urllib.parse import urlparse
+import os
+ 
+inUrl = input() #Можно eval, можно сделать вообще в одну строчку типа inUrl = tuple(urlparse(input()))), но под конкретную задачу достаточно и инпута.
+urlStr = urlparse(inUrl)
+findStr = "<h1>"
+ 
+#print (urlStr)
+ 
+ 
+urlTuple = tuple(urlStr)
+print (urlTuple)
+def parseH1(inUrl,findStr):
+    listTag = []
+    inUrlFile = urllib.request.urlopen(inUrl)
+    htmlFile = inUrlFile.read()
+    htmlFileRez = htmlFile.decode('utf-8')
+    #print (htmlFileRez)
+    f = open ('rez_tmp.html', 'w')
+    for line in htmlFileRez:
+        f.write(line)
+    f = open ('rez_tmp.html', 'r')
+    for line in f.readlines():
+        if findStr in line:
+            lineStr = line.strip()
+            lineStr = lineStr[4:-5:]
+            listTag.append(lineStr)
+    f.close()
+    os.remove('rez_tmp.html')
+    #print (listTag)
+    finalStr = ' '.join(listTag)
+    return finalStr
+print (parseH1(inUrl,findStr))
 
 
 
